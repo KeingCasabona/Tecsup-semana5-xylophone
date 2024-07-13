c785fc45c24fe3f7f301c12b0abed3b8149da310
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-07-13 00:28:15 -0500

    feat: Add Logica2

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AudioPlayer player = AudioPlayer();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -36,31 +37,42 @@ class HomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: Container(
-              color: Colors.red,
-              child: TextButton(
-                onPressed: () {
-                  AudioPlayer player = AudioPlayer();
-                  player.play(AssetSource('audios/note1.wav'));
-                },
-                child: Text(''),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.blueAccent,
-              child: TextButton(
-                onPressed: () {
-                  AudioPlayer player = AudioPlayer();
-                  player.play(AssetSource('audios/note2.wav'));
-                },
-                child: Text(''),
-              ),
-            ),
-          ),
+          Xylo(player: player, color: Colors.red, number: 1),
+          Xylo(player: player, color: Colors.orange, number: 2),
+          Xylo(player: player, color: Colors.blue, number: 3),
+          Xylo(player: player, color: Colors.pinkAccent, number: 4),
+          Xylo(player: player, color: Colors.green, number: 5),
+          Xylo(player: player, color: Colors.yellow, number: 6),
+          Xylo(player: player, color: Colors.purple, number: 7),
         ],
+      ),
+    );
+  }
+}
+
+class Xylo extends StatelessWidget {
+  const Xylo({
+    super.key,
+    required this.player,
+    required this.color,
+    required this.number,
+  });
+
+  final AudioPlayer player;
+  final Color color;
+  final int number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            player.play(AssetSource('audios/note$number.wav'));
+          },
+          child: Text(''),
+        ),
       ),
     );
   }
